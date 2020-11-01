91b2890b91c05d62817771196e50f0354ad2f50b
asd412id <asdar.binsyam@gmail.com>
2020-11-01 14:55:11 +0800

    Create release key

--- app/src/main/java/com/asd412id/jmeet/modules/ApiConnection.kt
+++ app/src/main/java/com/asd412id/jmeet/modules/ApiConnection.kt
@@ -3,6 +3,7 @@ package com.asd412id.jmeet.modules
 class ApiConnection {
     fun baseURL(): String {
 //        return "http://10.0.3.2:8000/api/v1/"
-        return "http://192.168.3.93:8000/api/v1/"
+//        return "http://192.168.3.93:8000/api/v1/"
+        return "https://jmeet.webarsip.com/api/v1/"
     }
 }
